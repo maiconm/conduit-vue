1a26b1e676607cf745c5741b0243bd11d7dd78fe
maiconm <maiconm23@gmail.com>
2020-12-06 09:00:30 -0300

    Cria o componente Header em branco.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
+    Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    I can bring you back to life, but I can't make you live
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
