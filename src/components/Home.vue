41c6a9b9b8e53712b9e7c7bfcd4bf27c19c75205
maiconm <maiconm23@gmail.com>
2020-12-06 11:00:18 -0300

    Introduz barra de navegação de feeds.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="center bg-green">
-    <h1 id="logo">
-      conduit
-    </h1>
-    <p id="lead">
-      Um lugar para você compartilhar todo o seu conhecimento!
-    </p>
+  <div>
+    <div class="center bg-green">
+      <h1 id="logo">
+        conduit
+      </h1>
+      <p id="lead">
+        Um lugar para você compartilhar todo o seu conhecimento!
+      </p>
+    </div>
+    <div class="center">
+      <div class="row sm-vertical">
+        <div class="col sm-full">
+          <nav>
+            <router-link :to="{ name: 'my-feed' }">
+              Meu feed
+            </router-link>
+            <router-link :to="{ name: 'global-feed' }">
+              Global feed
+            </router-link>
+          </nav>
+          <main>
+            <router-view />
+          </main>
+        </div>
+        <aside
+          class="no-flex sm-full"
+          style="width: 260px;"
+        >
+          Tags populares
+        </aside>
+      </div>
+    </div>
   </div>
 </template>
 
