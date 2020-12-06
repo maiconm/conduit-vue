0af72cd24bd2920aea1e13358bc62bf598488abe
maiconm <maiconm23@gmail.com>
2020-12-06 09:29:46 -0300

    Introduz conteúdo inicial e estilo do componente Home.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <Header />
+    <Home />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
+    Home,
     Header
   }
 }
